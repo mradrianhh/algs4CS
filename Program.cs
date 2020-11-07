5d2f3a0ac3b0b935205298390c97402b4f213a63
Adrian Hardy Hansen <hansen.adrian.hardy@gmail.com>
2020-11-07 14:36:29 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace algs4CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
